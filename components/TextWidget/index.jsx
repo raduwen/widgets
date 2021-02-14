46d2224df0fb6c68ca90400e7b3739f5c59d83c7
raduwen <raduwen@gmail.com>
2021-02-14 13:40:16 +0900

    implements TextWidget

---
+++ components/TextWidget/index.jsx
@@ -0,0 +1,30 @@
+const TextWidget = ({ text }) => {
+  const edge_color = 'black';
+  const ew = 1;
+  const edge = [
+    `${ew}px ${ew}px 0 ${edge_color}`,
+    `-${ew}px -${ew}px 0 ${edge_color}`,
+    `-${ew}px ${ew}px 0 ${edge_color}`,
+    `${ew}px -${ew}px 0 ${edge_color}`,
+    `0px ${ew}px 0 ${edge_color}`,
+    `0-${ew}px 0 ${edge_color}`,
+    `-${ew}px 0 0 ${edge_color}`,
+    `${ew}px 0 0 ${edge_color}`
+  ].join(', ');
+
+  const style = {
+    position: 'absolute',
+    right: 0,
+    width: '320px',
+    height: '540px',
+    padding: '4px 8px',
+    color: 'white',
+    textShadow: edge,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    whiteSpace: 'pre-wrap',
+  };
+
+  return <div style={style}>{text}</div>;
+};
+
+export default TextWidget
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import Head from 'next/head';
 
+import TextWidget from '../components/TextWidget'
 import TimeWidget from '../components/TimeWidget'
 
 const IndexPage = () => {
+  const text = `オレオレOBSウィジュエットの整理`;
   return (
     <div>
+      <TextWidget text={text} />
       <TimeWidget size={30} />
     </div>
   );
