32f3738a9a92dcac9fa5d301395b0bb8bbe291bc
raduwen <raduwen@gmail.com>
2021-02-15 14:12:08 +0900

    introduce put dynamic widget

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,25 @@ import Head from 'next/head';
 import TextWidget from '@/components/TextWidget';
 import TimeWidget from '@/components/TimeWidget';
 
+const Widgets = {
+  'text': TextWidget,
+  'time': TimeWidget,
+};
+
 const IndexPage = () => {
   const text = `オレオレOBSウィジュエットの整理`;
+
+  const widgets = [
+    { name: 'text', props: { text: text } },
+    { name: 'time', props: { size: 30 } },
+  ];
+
   return (
     <div>
-      <TextWidget text={text} />
-      <TimeWidget size={30} />
+      {widgets.map((widget) => {
+        const Widget = Widgets[widget.name];
+        return <Widget {...widget.props} />
+      })}
     </div>
   );
 };
