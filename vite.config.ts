6c811cd2f6c716383c80a43147e3d56c74b4a862
raduwen <raduwen@gmail.com>
2021-08-27 08:36:10 +0900

    fix preview and admin structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
+import mpa from 'vite-plugin-mpa';
+
+var plugins = [tsconfigPaths()];
+
+if (process.env.NODE_ENV !== "production") {
+  plugins.push(reactRefresh());
+  plugins.push(mpa({
+      scanDir: '.'
+    })
+  );
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin/index.html'),
+        preview: 'preview/index.html',
+        admin: 'admin/index.html'
       }
     }
   },
-  plugins: [tsconfigPaths(), reactRefresh()]
+  plugins,
 });
